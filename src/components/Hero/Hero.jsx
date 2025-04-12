de22728d9c162a71657bc6e1e2253ced3d7b4e96
Meng Sophai <mengsophai7799@gmail.com>
2025-04-12 16:22:08 +0700

    update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,47 +7,7 @@ const Hero = () => {
 
   return (
     <div className="hero">
-      <div className="framers">
-        {['LOVE', 'Phai'].map((letter, index) => (
-          <motion.div
-            key={index}
-            style={{
-              width: 50,
-              height: 50,
-              padding: 20,
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              color: '#fff',
-              fontWeight: 'bold',
-              fontSize: '16px',
-              background: 'linear-gradient(267deg, #Da7c25 0.36%, #b923e1 102.06%)',
-              border: '4px solid #fff',
-              margin: '20px auto',
-              borderRadius: '10px',
-              boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
-            }}
-            animate={{
-              scale: [1, 2, 2, 1, 1],
-              rotate: [0, 0, 180, 180, 0],
-              borderRadius: ["10%", "10%", "50%", "50%", "10%"],
-            }}
-            transition={{
-              duration: 2,
-              ease: "easeInOut",
-              times: [0, 0.2, 0.5, 0.8, 1],
-              repeat: Infinity,
-              repeatDelay: 1,
-            }}
-          >
-            {letter}
-          </motion.div>
-          
-
-          
-        ))}
-       
-      </div>  
+  
       <div className="image-wrapper">
         <motion.img
           src={phaixtey}
